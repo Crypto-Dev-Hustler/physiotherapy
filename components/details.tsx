7bab4bf92b75ae7ee926a30b37e4d46be7c5351d
Ayushman411214 <ayushman311214@gmail.com>
2025-06-28 11:28:53 +0530

    5 year

--- components/details.tsx
+++ components/details.tsx
@@ -1,20 +1,23 @@
-"use client"
+"use client";
 
 export function Details() {
-    return (
-        <section id="details" className="flex flex-row items-center justify-center gap-16 py-10 bg-gray-900 text-white">
-            <div className="text-center lg:mr-80 lg:text-4xl text-grey-900">
-                3 yrs+
-                <div className="lg:text-2xl font-bold">Experience</div>
-            </div>
-            <div className="text-center lg:text-4xl text-grey-900">
-                50+
-                <div className="lg:text-2xl font-bold">Therapies</div>
-            </div>
-            <div className="text-center lg:ml-80 lg:text-4xl text-grey-900">
-                4000+
-                <div className="lg:text-2xl font-bold">Satisfied Patients</div>
-            </div>
-        </section>
-    )
+  return (
+    <section
+      id="details"
+      className="flex flex-row items-center justify-center gap-16 py-10 bg-gray-900 text-white"
+    >
+      <div className="text-center lg:mr-80 lg:text-4xl text-grey-900">
+        5 yrs+
+        <div className="lg:text-2xl font-bold">Experience</div>
+      </div>
+      <div className="text-center lg:text-4xl text-grey-900">
+        50+
+        <div className="lg:text-2xl font-bold">Therapies</div>
+      </div>
+      <div className="text-center lg:ml-80 lg:text-4xl text-grey-900">
+        4000+
+        <div className="lg:text-2xl font-bold">Satisfied Patients</div>
+      </div>
+    </section>
+  );
 }
