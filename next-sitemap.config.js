bb6fa08b7c1b19c4df377a9d8ee22b27d758c03f
Ayushman411214 <ayushman311214@gmail.com>
2025-07-13 08:26:45 +0530

    Configure Sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "http://www.painfreerehabcenter.in",
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  sitemapSize: 5000,
+};
