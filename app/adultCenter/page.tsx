1ca60ead7eed3a630785d2990ee87e6ffa702356
Ayushman411214 <ayushman311214@gmail.com>
2025-07-13 10:35:09 +0530

    a

--- app/adultCenter/page.tsx
+++ app/adultCenter/page.tsx
@@ -1,29 +1,22 @@
-"use client"
+"use client";
 
-import { AdultTherapiesSection } from "@/components/adultComponents/adultServices"
-import { AdultFooter } from "@/components/adultComponents/adultFooter"
-import { AdultDoctorsSection } from "@/components/adultComponents/adultDoctors"
-import { AdultDetailSection } from "@/components/adultComponents/adultDetails"
-import { AdultHero } from "@/components/adultComponents/adultHero"
-import { AdultNavbar } from "@/components/adultComponents/adultNavbar"
+import { AdultTherapiesSection } from "@/components/adultComponents/adultServices";
+import { AdultFooter } from "@/components/adultComponents/adultFooter";
+import { AdultDoctorsSection } from "@/components/adultComponents/adultDoctors";
+import { AdultDetailSection } from "@/components/adultComponents/adultDetails";
+import { AdultHero } from "@/components/adultComponents/adultHero";
+import { AdultNavbar } from "@/components/adultComponents/adultNavbar";
 // app/about/page.tsx
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Adultcenter| PainFree Rehab Center",
-  description: "Learn more about our expert physiotherapy services in Meerut and how we help patients recover fast.",
-};
-
 
 export default function Adult() {
-    return(
-        <div>
-            <AdultNavbar />
-            <AdultHero />
-            <AdultDetailSection />
-            <AdultTherapiesSection />
-            <AdultDoctorsSection />
-            <AdultFooter />
-        </div>
-    )
+  return (
+    <div>
+      <AdultNavbar />
+      <AdultHero />
+      <AdultDetailSection />
+      <AdultTherapiesSection />
+      <AdultDoctorsSection />
+      <AdultFooter />
+    </div>
+  );
 }
