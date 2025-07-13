cff34cb2ecb8204b7a4157b8a0b5609a61040d43
Ayushman411214 <ayushman311214@gmail.com>
2025-07-13 08:42:55 +0530

    Configure google consolr

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
   title: "Physiotherapy Center",
   description:
     "Expert physiotherapy services focused on pain relief, injury recovery, and physical wellness. Personalized treatment plans by certified professionals.",
+  verification: {
+    google: "gKoDb2boaAkFO_lnt6v_Tx1sGgFl4V5V29-6bihQlJc",
+  },
 };
 
 export default function RootLayout({
