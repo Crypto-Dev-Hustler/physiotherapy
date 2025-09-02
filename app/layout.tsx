90bf977d9248893da519ea12593ba39f9dea9ca2
Ayushman411214 <ayushman311214@gmail.com>
2025-09-02 22:12:43 +0530

    optimaization

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,12 @@ export const metadata: Metadata = {
     keywords:
       "physiotherapy, pain relief, injury recovery, physical therapy,Gurgaon physiotherapy, back pain treatment, neck pain therapy, shoulder pain rehab, slip disc therapy, sports physiotherapy, post-surgery rehabilitation, neurological physiotherapy, home physiotherapy services, best physiotherapy, best physiotherapist Gurgaon, pain free rehab center, PainFree clinic , physiotherapy near me,physiotherapy gurgaon,physiotherapist near me,painfree rehab & physiotherapy center – dr. priyanka kashyap,physiotherapy in sector 46 gurgaon,physiotherapist gurgaon,physiotherapist in gurgaon,physiotherapy clinic in gurgaon,physio rehab centre near me,best physiotherapist near me,female physiotherapist in gurgaon,rehab in gurgaon,dr priyanka physiotherapist,best child physiotherapist near me,painfree physiotherapy and sports injuries clinic,top physiotherapy near me,lower back pain physiotherapy",
   },
+  openGraph: {
+    title: "PainFree Rehab & Physiotherapy Center",
+    siteName: "PainFree Rehab & Physiotherapy Center",
+    url: "https://www.painfreerehabcenter.in",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
